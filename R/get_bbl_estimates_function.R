a6e3d95ee35722d59f17a6c6e7ef892c6b08ce00
jwucouncil <61881283+jwucouncil@users.noreply.github.com>
2024-02-21 15:57:50 -0500

    councilcount migration

---
+++ R/get_bbl_estimates_function.R
@@ -0,0 +1,11 @@
+#' Function to retrieve PLUTO bbl population estimates
+#'
+#' @import dplyr
+#' @return tibble with PLUTO bbl population estimates
+#' @export
+#'
+
+get_bbl_estimates <- function() {
+  readr::read_csv(fs::path_package("extdata", glue::glue("bbl-population-estimates_2021.csv"), package = "councilcount")) %>%
+    janitor::clean_names()
+  }
