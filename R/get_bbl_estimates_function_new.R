3c859787b6681cd7a63fcdad9afc58fd413460c9
Rachel Avram <75150847+ravram193@users.noreply.github.com>
2024-11-07 17:04:29 -0500

    Add files via upload

--- R/get_bbl_estimates_function_new.R
+++ R/get_bbl_estimates_function_new.R
@@ -1,27 +1,31 @@
 #' A dataframe containing BBL-level population estimates for a specified year
 #'
+#' @import tidyverse
 #' @import dplyr
 #' @param year (string). The desired year for BBL-level estimates
 #' @return tibble with PLUTO bbl population estimates
 #' @export
 #'
 
-acs_years <- c('2011', '2016', '2021')
-
 get_bbl_estimates <- function(year) {
 
-  # Warning message if unavailable survey year selected
-  if (!(year %in% acs_years)) {
-    message("This year is not available.", "\n",
-            "Please choose from the following:\n",
-            paste0('"', acs_years, '"', collapse = "\n"))
-  }
+  # find all the available years
+  csv_names <- dir(system.file("extdata", package = "councilcount"))
+  bbl_csv_names <- csv_names[grepl("bbl-population-estimates_", csv_names)]
+  bbl_years <- sapply(bbl_csv_names, function(x) substr(x, 26, 29))
 
-  # Construct the name of the dataset based on the year
+  # construct the name of the dataset based on the year
   bbl_name <- paste0("bbl-population-estimates_", year, '.csv')
 
-  # Retrieve the dataset from the package
+  # error message if unavailable survey year selected
+  if (!(year %in% bbl_years)) {
+    stop("This year is not available.", "\n",
+         "Please choose from the following:\n",
+         paste0('"', bbl_years, '"', collapse = "\n"))
+  }
+
+  # retrieve the dataset from the package
   readr::read_csv(fs::path_package("extdata", glue::glue(bbl_name), package = "councilcount")) %>%
     janitor::clean_names()
 
-  }
+}
